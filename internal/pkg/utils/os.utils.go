f174e3e032032bf73d25ca99a4db90a2386367f6
lotsman <67526489+sujer-ux@users.noreply.github.com>
2025-08-19 23:59:01 +0500

    Launch simple
    https://github.com/lotos-linux/hypr-dock/issues/15

--- internal/pkg/utils/os.utils.go
+++ internal/pkg/utils/os.utils.go
@@ -7,57 +7,13 @@ import (
 	"os/exec"
 	"strings"
 )
-
 func Launch(command string) {
-	if strings.Contains(command, "\"") {
-		command = strings.ReplaceAll(command, "\"", "")
-	}
-
-	badArg := strings.Index(command, "%")
-	if badArg != -1 {
-		command = command[:badArg-1]
-	}
-
-	elements := strings.Split(command, " ")
-
-	// find prepended env variables, if any
-	envVarsNum := strings.Count(command, "=")
-	var envVars []string
-
-	cmdIdx := -1
-
-	if envVarsNum > 0 {
-		for idx, item := range elements {
-			if strings.Contains(item, "=") {
-				envVars = append(envVars, item)
-			} else if !strings.HasPrefix(item, "-") && cmdIdx == -1 {
-				cmdIdx = idx
-			}
-		}
-	}
-	if cmdIdx == -1 {
-		cmdIdx = 0
-	}
-	var args []string
-	for _, arg := range elements[1+cmdIdx:] {
-		if !strings.Contains(arg, "=") {
-			args = append(args, arg)
-		}
-	}
-
-	cmd := exec.Command(elements[cmdIdx], elements[1+cmdIdx:]...)
-
-	// set env variables
-	if len(envVars) > 0 {
-		cmd.Env = os.Environ()
-		cmd.Env = append(cmd.Env, envVars...)
-	}
-
-	log.Printf("env vars: %s; command: '%s'; args: %s\n", envVars, elements[cmdIdx], args)
-
-	if err := cmd.Start(); err != nil {
-		log.Println("Unable to launch command!", err.Error())
-	}
+    cmd := exec.Command("sh", "-c", command)
+    log.Printf("Launching command: %s\n", command)
+    
+    if err := cmd.Start(); err != nil {
+        log.Printf("Unable to launch command: %s, error: %v\n", command, err)
+    }
 }
 
 func LoadTextFile(path string) ([]string, error) {

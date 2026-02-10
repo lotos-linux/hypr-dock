0fbfc679949808e9faa90e65040316887171f8dc
sujer-ux <mat123matvei.123@gmail.com>
2026-02-10 06:10:19 +0500

    fix alttab settings api usage

--- cmd/hypr-alttab/main.go
+++ cmd/hypr-alttab/main.go
@@ -12,7 +12,7 @@ func main() {
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	dev := flag.Bool("dev", false, "enable developer mode")
 
-	configDir := settings.GetConfigDir(*dev)
+	configDir, _, _ := settings.GetConfigDir(*dev)
 	configPath := filepath.Join(configDir, "hypr-alttab.conf")
 
 	switcher.Run(configPath)
